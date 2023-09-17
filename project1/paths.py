93f4cb215609c838e003994e28997714449373ff
Nil <nilbiescas3@gmail.com>
2023-09-17 18:29:10 +0200

    Añadidio paths.py pero si veo que no es buena idea lo acabare quitando tambien he hecho unas pruebas con unas iamages

---
+++ project1/paths.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+
+HERE = Path(os.path.dirname(os.path.abspath("__file__")))
+
+CAR_IMAGES_DIR = Path(r'C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (4)\images')
