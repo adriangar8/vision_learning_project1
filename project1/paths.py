58725e0e25fd9cc151d5ab8e5c0dac413fc520be
Nil <nilbiescas3@gmail.com>
2023-09-17 18:30:54 +0200

    Teneis dentro del paths el link al dataset para emepzar a probar

--- project1/paths.py
+++ project1/paths.py
@@ -3,4 +3,6 @@ from pathlib import Path
 
 HERE = Path(os.path.dirname(os.path.abspath("__file__")))
 
+# los imagenes son de aqui: https://www.kaggle.com/datasets/andrewmvd/car-plate-detection?resource=download
+
 CAR_IMAGES_DIR = Path(r'C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (4)\images')
