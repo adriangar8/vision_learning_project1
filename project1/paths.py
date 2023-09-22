158db46cd354c7b605fcbd612f85382fbb33d356
Nil <nilbiescas3@gmail.com>
2023-09-22 09:58:41 +0200

    update

--- project1/paths.py
+++ project1/paths.py
@@ -5,4 +5,4 @@ HERE = Path(os.path.dirname(os.path.abspath("__file__")))
 
 # los imagenes son de aqui: https://www.kaggle.com/datasets/andrewmvd/car-plate-detection?resource=download
 
-CAR_IMAGES_DIR = Path(r'C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (4)\images')
+CAR_IMAGES_DIR = Path(r'C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (4)\images') 
