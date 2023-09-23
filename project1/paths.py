4a9486acbda16726f3bc9fca0c61342467877dbd
Nil Biescas Rue <nilbiescas3@gmail.com>
2023-09-23 18:34:35 +0200

    Update

--- project1/paths.py
+++ project1/paths.py
@@ -2,15 +2,12 @@ import os
 from pathlib import Path
 
 HERE = Path(os.path.dirname(os.path.abspath("__file__")))
-TEST = Path(r"C:\Users\34644\Desktop\Firts Semester\Vision and learning\Test")
+TEST = Path(r'D:\NIL\Uni\licensplates\Test')
 # los imagenes son de aqui: https://www.kaggle.com/datasets/andrewmvd/car-plate-detection?resource=download
 
-CAR_IMAGES_DIR = Path(r'C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (4)\images') 
-
-DATASET = Path(r"C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (5)")
-
+#CAR_IMAGES_DIR = Path(r'D:\NIL\Uni\licensplates\Test') 
 
 FRONTAL = TEST / 'Frontal'
 LATERAL = TEST / 'Lateral'
 
-DATASET_IMG = DATASET / "images"
+DATASET_IMG = Path(r"D:\NIL\Uni\licensplates\images")
