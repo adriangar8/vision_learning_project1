d149d17a698275525680a361e457987cfdc62699
xsotopi <xaviminisoto@gmail.com>
2023-09-26 18:43:50 +0200

    update

--- project1/paths.py
+++ project1/paths.py
@@ -3,6 +3,17 @@ from pathlib import Path
 
 HERE = Path(os.path.dirname(os.path.abspath("__file__")))
 
+TEST = Path(r'C:\Users\xavim\Desktop\License_plates\test_images\Test\Test')
 # los imagenes son de aqui: https://www.kaggle.com/datasets/andrewmvd/car-plate-detection?resource=download
 
-CAR_IMAGES_DIR = Path('/home/adrian/Documents/academia/UAB/year_3/vision_learning/project1/archive/images')
+
+FRONTAL = TEST / 'Frontal\Frontal'
+LATERAL = TEST / 'Lateral\Lateral'
+
+DATASET_IMG = Path(r"C:\Users\xavim\Desktop\Datos_vision\archive (5)")
+DATASET_CARS = Path(r"C:\Users\xavim\Desktop\Datos_vision\archive (4)")
+
+
+
+
+
