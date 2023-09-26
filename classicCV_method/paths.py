f3dfc2e4c928888a19623a8bb33928e0299f024b
Adrián <adrian.garcia8t@gmail.com>
2023-09-26 23:10:46 +0200

    changed structure

---
+++ classicCV_method/paths.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+HERE = Path(os.path.dirname(os.path.abspath("__file__")))
+
+# los imagenes son de aqui: https://www.kaggle.com/datasets/andrewmvd/car-plate-detection?resource=download
+
+CAR_IMAGES_DIR = Path('/home/adrian/Documents/academia/UAB/year_3/vision_learning/project1/archive/images')
