656d6f9e05641ad3d1296ea5d03062bc9e239e45
Nil <nilbiescas3@gmail.com>
2023-10-09 15:33:06 +0200

    Update

--- project1/MACHINE/paths.py
+++ project1/MACHINE/paths.py
@@ -5,11 +5,14 @@ HERE = Path(os.path.dirname(os.path.abspath("__file__")))
 TEST = Path(r'C:\Users\34644\Desktop\Firts Semester\Vision and learning\Test')
 # los imagenes son de aqui: https://www.kaggle.com/datasets/andrewmvd/car-plate-detection?resource=download
 
-#CAR_IMAGES_DIR = Path(r'D:\NIL\Uni\licensplates\Test') 
+ 
 
 FRONTAL = TEST / 'Frontal'
 LATERAL = TEST / 'Lateral'
 
-DATASET_IMG = Path(r"C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (5)")
-DATASET_CARS = Path(r"C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (4)")
+# You need to put here your paths to the datasets
+# DATASET_ARCHIVE_4 = https://www.kaggle.com/datasets/andrewmvd/car-plate-detection?resource=download
+DATASET_ARCHIVE_4 = Path(r"C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (4)")
 
+# DATASET_ARCHIVE_4 =
+DATASET_ARCHIVE_5 = Path(r"C:\Users\34644\Desktop\Firts Semester\Vision and learning\archive (5)")
